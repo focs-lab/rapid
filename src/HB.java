ae194d0e8acf15479dc95ceba76552fd1c846d32
Umang Mathur <umathur3@illinois.edu>
2018-06-23 20:27:30 -0500

    Printing time

--- src/HB.java
+++ src/HB.java
@@ -10,17 +10,11 @@ public class HB {
 	
 	public static void main(String[] args) {		
 		CmdOptions options = new GetOptions(args).parse();
-		//System.out.println(options);
-		/*
 		long startTimeAnalysis = System.nanoTime();
-		*/
 		HBEngine engine = new HBEngine(options.parserType, options.path);
 		engine.analyzeTrace(options.multipleRace, options.verbosity);
-		/*
 		long stopTimeAnalysis = System.nanoTime();
 		long timeAnalysis = stopTimeAnalysis - startTimeAnalysis;
 		System.out.println("Time for analysis = " + timeAnalysis + " nanoseconds");
-		*/
-		//System.out.println(options);
 	}
 }
