cf1c81861ae71bc7e1d23d5688e658d4f2a3facf
Umang Mathur <umathur3@illinois.edu>
2020-10-05 00:17:27 -0500

    PrintCompletionStatus in Engine

--- src/engine/Engine.java
+++ src/engine/Engine.java
@@ -9,36 +9,36 @@ import util.trace.Trace;
 
 public abstract class Engine<E extends Event> {
 	protected ParserType parserType;
-	protected Trace trace; //CSV
-	protected ParseRVPredict rvParser;//RV
-	protected ParseStandard stdParser; //STD
-	protected ParseRoadRunner rrParser; //RR
+	protected Trace trace; // CSV
+	protected ParseRVPredict rvParser;// RV
+	protected ParseStandard stdParser; // STD
+	protected ParseRoadRunner rrParser; // RR
 	protected E handlerEvent;
-	
-	public Engine(ParserType pType){
+
+	public Engine(ParserType pType) {
 		this.parserType = pType;
 	}
-	
-	protected void initializeReader(String trace_folder){
-		if(this.parserType.isRV()){
+
+	protected void initializeReader(String trace_folder) {
+		if (this.parserType.isRV()) {
 			initializeReaderRV(trace_folder);
-		}
-		else if(this.parserType.isCSV()){
+		} else if (this.parserType.isCSV()) {
 			initializeReaderCSV(trace_folder);
-		}
-		else if(this.parserType.isSTD()){
+		} else if (this.parserType.isSTD()) {
 			initializeReaderSTD(trace_folder);
-		}
-		else if(this.parserType.isRR()){
+		} else if (this.parserType.isRR()) {
 			initializeReaderRR(trace_folder);
 		}
 	}
-	
+
 	protected abstract void initializeReaderRV(String trace_folder);
-	
+
 	protected abstract void initializeReaderCSV(String trace_file);
-	
+
 	protected abstract void initializeReaderSTD(String trace_file);
-	
+
 	protected abstract void initializeReaderRR(String trace_file);
+
+	protected void printCompletionStatus() {
+	}
 }
