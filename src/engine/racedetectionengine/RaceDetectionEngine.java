c91026f231cd5ea14eb390cbaf28968397c2b958
Umang Mathur <umathur3@illinois.edu>
2020-10-05 00:16:48 -0500

    Refactor and printCompletionStatus in RaceDetectionEngine

--- src/engine/racedetectionengine/RaceDetectionEngine.java
+++ src/engine/racedetectionengine/RaceDetectionEngine.java
@@ -11,33 +11,41 @@ import parse.rv.ParseRVPredict;
 import parse.std.ParseStandard;
 import util.trace.TraceAndDataSets;
 
-public abstract class RaceDetectionEngine<St extends State, RDE extends RaceDetectionEvent<St>> extends Engine<RDE> {
+public abstract class RaceDetectionEngine<St extends State, RDE extends RaceDetectionEvent<St>>
+		extends Engine<RDE> {
 
 	public St state;
 	protected HashSet<Thread> threadSet;
 
+	protected long eventCount;
+	protected Long raceCount;
+	protected Long totalSkippedEvents;
+
+	protected boolean enablePrintStatus;
+
 	public RaceDetectionEngine(ParserType pType) {
 		super(pType);
+		enablePrintStatus = true;
 	}
-	
+
 	@Override
-	protected void initializeReaderRV(String trace_folder){
+	protected void initializeReaderRV(String trace_folder) {
 		rvParser = new ParseRVPredict(trace_folder, this.threadSet);
 	}
 
 	@Override
-	protected void initializeReaderCSV(String trace_file){
+	protected void initializeReaderCSV(String trace_file) {
 		TraceAndDataSets traceAndDataSets = ParseCSV.parse(true, trace_file);
 		this.threadSet = traceAndDataSets.getThreadSet();
 		this.trace = traceAndDataSets.getTrace();
 	}
-	
+
 	@Override
 	protected void initializeReaderSTD(String trace_file) {
 		stdParser = new ParseStandard(trace_file, true);
 		threadSet = stdParser.getThreadSet();
 	}
-	
+
 	@Override
 	protected void initializeReaderRR(String trace_file) {
 		rrParser = new ParseRoadRunner(trace_file, true);
@@ -48,86 +56,79 @@ public abstract class RaceDetectionEngine<St extends State, RDE extends RaceDete
 
 	protected abstract void postHandleEvent(RDE handlerEvent);
 
-	protected boolean analyzeEvent(RDE handlerEvent, int verbosity, Long eventCount){
+	protected boolean analyzeEvent(RDE handlerEvent, int verbosity, Long eventCount) {
 		boolean raceDetected = false;
-		try{
+		try {
 			raceDetected = handlerEvent.Handle(state, verbosity);
-		}
-		catch(OutOfMemoryError oome){
+		} catch (OutOfMemoryError oome) {
 			oome.printStackTrace();
 			System.err.println("Number of events = " + Long.toString(eventCount));
 			state.printMemory();
 		}
-		if(verbosity==3 && handlerEvent.getType().isAccessType() ){
-			System.out.println("|"+Long.toString(eventCount));
+		if (verbosity == 3 && handlerEvent.getType().isAccessType()) {
+			System.out.println("|" + Long.toString(eventCount));
 		}
 
 		if (raceDetected) {
-			//raceCount ++;
+			// raceCount ++;
 			System.out.println(handlerEvent.getLocId());
 
 		}
 		return raceDetected;
 	}
 
-	public void analyzeTrace(boolean multipleRace, int verbosity){
-		if(this.parserType.isRV()){
-			analyzeTraceRV(multipleRace, verbosity);		
-		}
-		else if(this.parserType.isCSV()){
+	public void analyzeTrace(boolean multipleRace, int verbosity) {
+		eventCount = (long) 0;
+		raceCount = (long) 0;
+		totalSkippedEvents = (long) 0;
+		if (this.parserType.isRV()) {
+			analyzeTraceRV(multipleRace, verbosity);
+		} else if (this.parserType.isCSV()) {
 			analyzeTraceCSV(multipleRace, verbosity);
-		}
-		else if(this.parserType.isSTD()){
+		} else if (this.parserType.isSTD()) {
 			analyzeTraceSTD(multipleRace, verbosity);
-		}
-		else if(this.parserType.isRR()){
+		} else if (this.parserType.isRR()) {
 			analyzeTraceRR(multipleRace, verbosity);
 		}
+		printCompletionStatus();
+		postAnalysis();
 	}
-	
-	public void analyzeTraceCSV(boolean multipleRace, int verbosity) {
-		int eventCount =  0;
-		Long raceCount = (long) 0;
-		Long totalSkippedEvents = (long) 0;
 
-		for(eventCount = 0; eventCount < trace.getSize(); eventCount ++){
-			handlerEvent.copyFrom(trace.getEventAt(eventCount));
-			if(skipEvent(handlerEvent)){
+	protected void postAnalysis() {
+	}
+
+	public void analyzeTraceCSV(boolean multipleRace, int verbosity) {
+		for (eventCount = 0; eventCount < trace.getSize(); eventCount++) {
+			handlerEvent.copyFrom(trace.getEventAt((int) eventCount));
+			if (skipEvent(handlerEvent)) {
 				totalSkippedEvents = totalSkippedEvents + 1;
-			}
-			else{
-				boolean raceDetected = analyzeEvent(handlerEvent, verbosity, (long) eventCount);
-				if(raceDetected){
-					raceCount ++;
-					if (!multipleRace){
+			} else {
+				boolean raceDetected = analyzeEvent(handlerEvent, verbosity, eventCount);
+				if (raceDetected) {
+					raceCount++;
+					if (!multipleRace) {
 						break;
 					}
 				}
 				postHandleEvent(handlerEvent);
 			}
-		}		
-		System.out.println("Analysis complete");
-		System.out.println("Number of 'racy' events found = " + Long.toString(raceCount));
+		}
 	}
 
 	public void analyzeTraceRV(boolean multipleRace, int verbosity) {
-		Long eventCount = (long) 0;
-		Long raceCount = (long) 0;
-		Long totalSkippedEvents = (long) 0;
-
-		if(rvParser.pathListNotNull()){
-			while(rvParser.hasNext()){
+		if (rvParser.pathListNotNull()) {
+			while (rvParser.hasNext()) {
 				eventCount = eventCount + 1;
 				rvParser.getNextEvent(handlerEvent);
 
-				if(skipEvent(handlerEvent)){
+				if (skipEvent(handlerEvent)) {
 					totalSkippedEvents = totalSkippedEvents + 1;
-				}
-				else{
-					boolean raceDetected = analyzeEvent(handlerEvent, verbosity, (long) eventCount);
-					if(raceDetected){
-						raceCount ++;
-						if (!multipleRace){
+				} else {
+					boolean raceDetected = analyzeEvent(handlerEvent, verbosity,
+							(long) eventCount);
+					if (raceDetected) {
+						raceCount++;
+						if (!multipleRace) {
 							break;
 						}
 					}
@@ -135,63 +136,58 @@ public abstract class RaceDetectionEngine<St extends State, RDE extends RaceDete
 				}
 			}
 		}
-		System.out.println("Analysis complete");
-		System.out.println("Number of 'racy' events found = " + Long.toString(raceCount));
 	}
 
 	public void analyzeTraceSTD(boolean multipleRace, int verbosity) {
-		Long eventCount = (long) 0;
-		Long raceCount = (long) 0;
-		Long totalSkippedEvents = (long) 0;
-
-		while(stdParser.hasNext()){
+		while (stdParser.hasNext()) {
 			eventCount = eventCount + 1;
 			stdParser.getNextEvent(handlerEvent);
 
-			if(skipEvent(handlerEvent)){
+			if (skipEvent(handlerEvent)) {
 				totalSkippedEvents = totalSkippedEvents + 1;
-			}
-			else{
-				boolean raceDetected = analyzeEvent(handlerEvent, verbosity, (long) eventCount);
-				if(raceDetected){
-					raceCount ++;
-					if (!multipleRace){
+			} else {
+				boolean raceDetected = analyzeEvent(handlerEvent, verbosity, eventCount);
+				if (raceDetected) {
+					raceCount++;
+					if (!multipleRace) {
 						break;
 					}
 				}
 				postHandleEvent(handlerEvent);
 			}
 		}
-		
-		System.out.println("Analysis complete");
-		System.out.println("Number of 'racy' events found = " + Long.toString(raceCount));
 	}
-	
+
 	public void analyzeTraceRR(boolean multipleRace, int verbosity) {
 		Long eventCount = (long) 0;
 		Long raceCount = (long) 0;
 		Long totalSkippedEvents = (long) 0;
 
-		while(rrParser.checkAndGetNext(handlerEvent)){
+		while (rrParser.checkAndGetNext(handlerEvent)) {
 			eventCount = eventCount + 1;
-			if(skipEvent(handlerEvent)){
+			if (skipEvent(handlerEvent)) {
 				totalSkippedEvents = totalSkippedEvents + 1;
-			}
-			else{
-				boolean raceDetected = analyzeEvent(handlerEvent, verbosity, (long) eventCount);
-				if(raceDetected){
-					raceCount ++;
-					if (!multipleRace){
+			} else {
+				boolean raceDetected = analyzeEvent(handlerEvent, verbosity,
+						(long) eventCount);
+				if (raceDetected) {
+					raceCount++;
+					if (!multipleRace) {
 						break;
 					}
 				}
 				postHandleEvent(handlerEvent);
 			}
 		}
-		
-		System.out.println("Analysis complete");
-		System.out.println("Number of 'racy' events found = " + Long.toString(raceCount));
 	}
 
+	@Override
+	protected void printCompletionStatus() {
+		if (enablePrintStatus) {
+			System.out.println("Analysis complete");
+			System.out.println(
+					"Number of 'racy' events found = " + Long.toString(raceCount));
+		}
+	}
 
 }
